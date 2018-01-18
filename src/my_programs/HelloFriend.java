cc353f00c20633e3bedac83038f795d370a2c9b3
Shakil Ahmed <shakilmsc@gmail.com>
2018-01-17 20:16:42 -0500

    Added two programs

---
+++ src/my_programs/HelloFriend.java
@@ -0,0 +1,11 @@
+package my_programs;
+
+public class HelloFriend {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hello Friends");
+System.out.println("This my program in 1001");
+	}
+
+}
---
+++ src/my_programs/Intigers.java
@@ -0,0 +1,20 @@
+package my_programs;
+
+public class Intigers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int shoes = 14;
+		int bags = 10; 
+		int car= 1;
+		int phones = 2;
+		int laptops= 2;
+
+		System.out.println ("I had " + bags + " bags.");
+		System.out.println ("I had " + shoes + " shoes.");
+		System.out.println ("I have " + car + " car.");
+		System.out.println ("I have " + phones + " phones.");
+		System.out.println ("I have " + laptops + " laptops.");
+	}
+
+}
