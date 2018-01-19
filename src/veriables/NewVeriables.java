16b688adf2d4072ba3ded94bab720daa97d8b5ba
Shakil Ahmed <shakilmsc@gmail.com>
2018-01-18 21:42:04 -0500

    added some code

---
+++ src/veriables/NewVeriables.java
@@ -0,0 +1,11 @@
+package veriables;
+
+public class NewVeriables {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("This is my new veriable project");
+System.out.println("There are many different type of variables in JAVA");
+	}
+
+}
