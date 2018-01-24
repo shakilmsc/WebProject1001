ce38468dc748920350256fa0eeb45efbb4a3e5ef
Shakil Ahmed <shakilmsc@gmail.com>
2018-01-23 19:18:11 -0500

    added some code

---
+++ src/basics/Basics1.java
@@ -0,0 +1,24 @@
+package basics;
+
+
+public class Basics1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// this step prints in the output
+		System.out.println("I navigated to home page");
+	
+		Basics2 m=new Basics2();
+		m.ValidateHeader();
+	
+	
+		
+			
+		//Classobject.methodname
+		
+	
+		
+	}
+	
+}
---
+++ src/basics/Basics2.java
@@ -0,0 +1,11 @@
+package basics;
+
+public class Basics2 {
+
+		public void ValidateHeader ()
+		{
+System.out.println("Headers link Validated");
+		
+		}
+		
+}
