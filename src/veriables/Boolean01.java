6474b909fd61f868c18ca2e1071c292c23e4ae6e
Shakil Ahmed <shakilmsc@gmail.com>
2018-01-28 16:06:59 -0500

    added some code

---
+++ src/veriables/Boolean01.java
@@ -0,0 +1,30 @@
+package veriables;
+
+// Working with Boolean type veriables
+
+public class Boolean01 {
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		boolean x, y;
+		x =true; y = false;
+		
+		System.out.println("x= " +x);
+		System.out.println("y= " +y);
+		
+		int x1, y1; boolean z1;
+		x1=15; y1=10; 
+		
+		System.out.println("x1>y1 is " + (x1>y1));
+		
+		z1 = y1>x1;
+		System.out.println("y1>x1 is " +z1);
+		
+		
+		
+	}
+
+}
